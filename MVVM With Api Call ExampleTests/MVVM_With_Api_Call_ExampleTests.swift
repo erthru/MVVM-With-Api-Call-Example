c80331d5f91b440f8bc5be25fcece54c1e89f09e
Suprianto D <ersaka96@gmail.com>
2019-06-22 17:02:26 +0800

    Initial Commit

---
+++ MVVM With Api Call ExampleTests/MVVM_With_Api_Call_ExampleTests.swift
@@ -0,0 +1,34 @@
+//
+//  MVVM_With_Api_Call_ExampleTests.swift
+//  MVVM With Api Call ExampleTests
+//
+//  Created by Suprianto Djamalu on 22/06/19.
+//  Copyright © 2019 Suprianto Djamalu. All rights reserved.
+//
+
+import XCTest
+@testable import MVVM_With_Api_Call_Example
+
+class MVVM_With_Api_Call_ExampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
